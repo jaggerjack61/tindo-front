02b70ec05d7fdccc959ebfa06e3a8d8725e8d210
Samuel Jarai <jarai.samuel@gmail.com>
2023-04-04 12:46:06 +0200

    changes to checkout comp

--- src/components/CheckOut.vue
+++ src/components/CheckOut.vue
@@ -33,7 +33,7 @@
 
 
           <div class="form-group mt-3">
-                  <textarea class="form-control" v-model="message" name="message" rows="5" placeholder="Enter your address"
+                  <textarea class="form-control" v-model="address" name="message" rows="5" placeholder="Enter your address"
                             required></textarea>
           </div>
           <div class="form-group mt-3">
@@ -52,14 +52,24 @@
               <input type="number" placeholder="eg 0771234567" class="form-control">
             </div>
           </div>
+          <div v-if="method === 'bank'">
+            <div class="form-group mt-3">
+              <p>After clicking the make payment button you will be redirected to the paynow portal where you will be able to complete
+              the payment process using your local RTGS\ZWL bank account. </p>
+            </div>
+          </div>
           <div class="my-3">
 
           </div>
           <div class="mt-5">
             <div class="text-secondary my-3">A copy of the transaction transcript will be sent your registered email.</div>
-            <button v-if="!loading" type="submit" class="btn btn-primary float-start">Make Payment</button>
-            <button v-if="loading" class="btn btn-lg text-white" style="background: #2dc997">Please wait..
-            </button>
+            <button v-if="method === 'none'"  class="btn btn-secondary float-start">Make Payment</button>
+            <div v-if="method !== 'none'">
+              <button v-if="!loading" type="submit" class="btn btn-primary float-start">Make Payment</button>
+              <button v-if="loading" class="btn btn-lg text-white" style="background: #2dc997">Please wait..
+              </button>
+            </div>
+
           </div>
         </form>
       </div>
@@ -81,8 +91,21 @@ export default {
     return {
       assets: process.env.VUE_APP_ASSETS,
       loading:false,
-      method:'none'
+      address:'',
+      method:'none',
+      phone:''
 
+    }
+  },
+  methods:{
+    createOrder() {
+      let order = {
+        email:store.state.user.email,
+        items:store.state.cart,
+        phone:this.phone,
+        network:this.method,
+        reference:''
+      };
     }
   }
 }
